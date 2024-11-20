c497f97b60020e8d9e86ede26b41885e785e5d74
Roshan Khatri <rvkhatri@amazon.com>
2024-11-20 22:45:57 +0000

    set command info
    Signed-off-by: Roshan Khatri <rvkhatri@amazon.com>

--- src/json/json.cc
+++ src/json/json.cc
@@ -3302,6 +3302,9 @@ extern "C" int ValkeyModule_OnLoad(ValkeyModuleCtx *ctx) {
     if (!set_command_info(ctx, "JSON.SET", -4, ks_read_write_update, 1, std::make_tuple(0, 1, 0))) {
         return VALKEYMODULE_ERR;
     }
+    if (!set_command_info(ctx, "JSON.MSET", -4, ks_read_write_update, 1, std::make_tuple(0, 1, 0))) {
+        return VALKEYMODULE_ERR;
+    }
     // Commands under RW + Insert
     if (!set_command_info(ctx, "JSON.ARRAPPEND", -4, ks_read_write_insert, 1, std::make_tuple(0, 1, 0))) {
         return VALKEYMODULE_ERR;
